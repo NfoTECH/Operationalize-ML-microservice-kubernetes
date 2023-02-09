a47da55e9832558312c1748fc78a90a8b566bf03
EC2 Default User <ec2-user@ip-172-31-13-91.ec2.internal>
2023-02-09 15:32:43 +0000

    Feat: Add config.yml file for circleci

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nfotech/ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag ml-microservice-kubernetes $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
